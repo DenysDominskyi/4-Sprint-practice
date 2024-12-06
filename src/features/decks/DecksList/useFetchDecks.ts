68e2632dee8ccf232e1fc31a6bcf4d68727a7cab
DenysDominskyi <denysdominskyi@gmail.com>
2024-12-06 16:18:20 +0100

    Sprint4 Week3. skeleton

--- src/features/decks/DecksList/useFetchDecks.ts
+++ src/features/decks/DecksList/useFetchDecks.ts
@@ -1,17 +1,22 @@
 import { useAppDispatch, useAppSelector } from '../../../app/store.ts'
 import { selectDecks } from '../decks-selectors.ts'
-import { useEffect } from 'react'
+import { useEffect, useLayoutEffect, useState } from 'react'
 import { fetchDecksTC } from '../decks-thunks.ts'
 
 export const useFetchDecks = () => {
   const dispatch = useAppDispatch()
   const decks = useAppSelector(selectDecks)
+  const [isLoading, setIsLosading] = useState(false)
 
-  useEffect(() => {
-    dispatch(fetchDecksTC())
+  useLayoutEffect(() => {
+    setIsLosading(true)
+    dispatch(fetchDecksTC()).finally(()=>{
+      setIsLosading(false)
+    })
   }, [dispatch])
 
   return {
     decks,
+    isLoading
   }
 }
